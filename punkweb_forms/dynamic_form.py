97199ec1f16e4bfbb614667ebfb8f722338f497a
jprice <jprice@izeni.com>
2024-06-21 21:47:17 -0600

    Improve form class generation

--- punkweb_forms/dynamic_form.py
+++ punkweb_forms/dynamic_form.py
@@ -1,6 +1,28 @@
 from django import forms
-from django.forms import widgets
 from punkweb_forms.models import Field, Form
+
+
+def get_field_class(type):
+    field_classes = {
+        "boolean": forms.BooleanField,
+        "char": forms.CharField,
+        "choice": forms.ChoiceField,
+        "date": forms.DateField,
+        "datetime": forms.DateTimeField,
+        "decimal": forms.DecimalField,
+        "duration": forms.DurationField,
+        "email": forms.EmailField,
+        "file": forms.FileField,
+        "filepath": forms.FilePathField,
+        "float": forms.FloatField,
+        "image": forms.ImageField,
+        "integer": forms.IntegerField,
+        "multiplechoice": forms.MultipleChoiceField,
+        "time": forms.TimeField,
+        "url": forms.URLField,
+        "uuid": forms.UUIDField,
+    }
+    return field_classes.get(type)
 
 
 def get_form_class(form_id):
@@ -9,28 +31,16 @@ def get_form_class(form_id):
 
     field_dict = {}
     for field in fields:
-        if field.type == "text":
-            field_dict[field.label.lower()] = forms.CharField(
-                label=field.label,
-                required=field.required,
-            )
-        elif field.type == "textarea":
-            field_dict[field.label.lower()] = forms.CharField(
-                label=field.label,
-                required=field.required,
-                widget=widgets.Textarea,
-            )
-        elif field.type == "number":
-            field_dict[field.label.lower()] = forms.IntegerField(
-                label=field.label,
-                required=field.required,
-            )
-        elif field.type == "date":
-            field_dict[field.label.lower()] = forms.DateField(
-                label=field.label,
-                widget=widgets.SelectDateWidget,
-                required=field.required,
-            )
+        klass = get_field_class(field.type)
+        field_dict[field.name] = klass(required=field.required)
+        field_kwargs = {
+            "required": field.required,
+            "label": field.label or None,
+            "help_text": field.help_text or None,
+            "disabled": field.disabled,
+        }
+        field_kwargs = {k: v for k, v in field_kwargs.items() if v is not None}
+        field_dict[field.name] = klass(**field_kwargs)
 
     DynamicForm = type("DynamicForm", (forms.Form,), field_dict)
 
